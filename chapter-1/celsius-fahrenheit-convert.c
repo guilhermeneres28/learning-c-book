e0570db060c55c531cd5468bd412bea2744e604e
Guilherme Neres <guilhermeneresbsb@gmail.com>
2019-06-26 15:27:43 -0300

    writing celsius and fahrenheit table converter to review variable and loop concepts

---
+++ chapter-1/celsius-fahrenheit-convert.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+
+/* Program print Fahrenheit - Celsius table */
+int main()
+{
+    int fahr, celsius;
+    int lower, upper, step;
+
+    lower = 0;
+    upper = 300;
+    step = 20;
+
+    fahr = lower;
+    while (fahr <= upper)
+    {
+        /* Convert fahrenheit to celsius formula: C = (5/9) (F - 32)  */
+        celsius = 5 * (fahr - 32) / 9;
+        printf("%d\t%d\n", fahr, celsius);
+        fahr = fahr + step;
+    }
+}
