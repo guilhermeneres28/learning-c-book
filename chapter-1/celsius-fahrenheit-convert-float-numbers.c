6bae6b390d16aeaddf660721bc65e23aba1d3710
Guilherme Neres <guilhermeneresbsb@gmail.com>
2019-06-26 17:58:10 -0300

    writing program to convert celsius to fahrenheit

--- chapter-1/celsius-fahrenheit-convert-float-numbers.c
+++ chapter-1/celsius-fahrenheit-convert-float-numbers.c
@@ -13,11 +13,12 @@ int main()
 
     fahr = lower;
 
+    printf("-- Program to convert fahr to celsius--\n");
+
     while (fahr <= upper)
     {
         celsius = (5.0/9.0) * (fahr - 32.0);
         printf("%3.0f %6.1f\n", fahr, celsius);
         fahr = fahr + step;
-    }
-    
+    }   
 }
---
+++ chapter-1/convert-celsius-to-fahrenheit.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+
+int main()
+{
+    float celsius, fahr;
+    int lower, upper, step, temp;
+
+    lower = 0;
+    upper = 200;
+    step = 20;
+
+    celsius = lower;
+
+    while (celsius <= upper)
+    {
+        temp = (celsius / 5) * 9;
+        fahr = temp + 32;
+        printf("%3.0f %6.1f\n", celsius, fahr);
+        celsius += step;
+    }
+    
+}
