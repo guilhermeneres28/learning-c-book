1c314cc887428ce4c26d0f1f2fd2e9952b2daae9
Guilherme Neres <guilhermeneresbsb@gmail.com>
2019-06-26 14:38:02 -0300

    writting firts program in c

---
+++ chapter-1/hello.c
@@ -0,0 +1,6 @@
+#include <stdio.h> // include standard c lib
+
+int main() // define a function named main that receives no arguments
+{
+    printf("hello, world\n"); // function from standard c lib
+}
