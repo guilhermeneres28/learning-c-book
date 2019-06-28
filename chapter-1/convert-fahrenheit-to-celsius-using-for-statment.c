1b91e1d2b34ce35862c7982403d31ca3c8257f1b
Guilherme Neres <guilhermeneresbsb@gmail.com>
2019-06-28 14:42:59 -0300

    Refactoring using symbolic constans to avoid magic number

--- chapter-1/convert-fahrenheit-to-celsius-using-for-statment.c
+++ chapter-1/convert-fahrenheit-to-celsius-using-for-statment.c
@@ -1,8 +1,12 @@
 #include <stdio.h>
 
+#define UPPER 300
+#define LOWER 0
+#define STEP 20
+
 int main()
 {
-    for(int fahr = 300; fahr >= 0; fahr = fahr - 20)
+    for(int fahr = UPPER; fahr >= LOWER; fahr = fahr - STEP)
     {
         printf("%3d\t %6.1f\n", fahr, (5.0/9.0) * (fahr - 32.0));
     }
